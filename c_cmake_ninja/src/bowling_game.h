ced69168f6c13d6db1db6398603c6fe4c0a0658e
Nishizaki <yutaka.w1986@gmail.com>
2019-03-06 23:43:10 +0900

    The First Test (Red)

---
+++ c_cmake_ninja/src/bowling_game.h
@@ -0,0 +1,8 @@
+#ifndef BOWLING_GAME_H
+#define BOWLING_GAME_H
+
+extern void bowling_game_init(void);
+extern void bowling_game_roll(int pins);
+extern int bowling_game_score(void);
+
+#endif
---
+++ c_cmake_ninja/src/bowling_game_test.c
@@ -0,0 +1,21 @@
+#include "bowling_game.h"
+
+#include <assert.h>
+#include <stdbool.h>
+
+
+static void test_gutter_game(){
+    int i;
+    bowling_game_init();
+    for(i=0; i<20; i++){
+        bowling_game_roll(0);
+    }
+    assert( bowling_game_score() == 0 && "test_gutter_game()" );
+
+}
+
+int main(){
+    test_gutter_game();
+    
+    return 0;
+}
