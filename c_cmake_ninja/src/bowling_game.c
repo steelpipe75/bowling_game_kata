458ea8a6c73baa05fbb21cedfc4789151f72411c
Nishizaki <yutaka.w1986@gmail.com>
2019-03-13 21:31:24 +0900

    The Third Test

--- c_cmake_ninja/src/bowling_game.c
+++ c_cmake_ninja/src/bowling_game.c
@@ -1,16 +1,34 @@
 #include "bowling_game.h"
 
-static int score;
+enum { max_rolls = 21 };
+static int rolls[max_rolls];
+static int current_roll;
 
 void bowling_game_init(void){
-    score = 0;
+    int i;
+    for(i=0; i<max_rolls; i++){
+        rolls[i] = 0;
+    }
+    current_roll = 0;
 }
 
 void bowling_game_roll(int pins){
-    score += pins;
+    rolls[current_roll++] = pins;
 }
 
 int bowling_game_score(){
+    int score = 0;
+    int i;
+    int frame;
+    i=0;
+    for(frame=0; frame<10; frame++){
+        if( (rolls[i] + rolls[i+1]) == 10 ){
+            score += 10 + rolls[i+2];
+            i += 2;
+        }else{
+            score += rolls[i] + rolls[i+1];
+            i += 2;
+        }
+    }
     return score;
 }
-
--- c_cmake_ninja/src/bowling_game_test.c
+++ c_cmake_ninja/src/bowling_game_test.c
@@ -11,12 +11,20 @@ static void roll_many(int n, int pins){
     }
 }
 
+static void test_one_spare(){
+    bowling_game_init();
+    bowling_game_roll(5);
+    bowling_game_roll(5);   /* spare */
+    bowling_game_roll(3);
+    roll_many(17, 0);
+    assert( bowling_game_score() == 16 && "test_one_spare()" );
+}
+
 static void test_gutter_game(){
     int i;
     bowling_game_init();
     roll_many(20, 0);
     assert( bowling_game_score() == 0 && "test_gutter_game()" );
-
 }
 
 static void test_all_ones(){
@@ -29,6 +37,7 @@ static void test_all_ones(){
 int main(){
     test_gutter_game();
     test_all_ones();
+    test_one_spare();
 
     return 0;
 }
