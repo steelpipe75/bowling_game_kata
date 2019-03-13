fcb6872423c5e398b193d207c2cb1ff6df80cace
Nishizaki <yutaka.w1986@gmail.com>
2019-03-13 21:57:14 +0900

    Finally

--- c_cmake_ninja/src/bowling_game_test.c
+++ c_cmake_ninja/src/bowling_game_test.c
@@ -8,30 +8,6 @@ static void roll_many(int n, int pins){
     }
 }
 
-static void test_perfect_game(){
-    bowling_game_init();
-    roll_many(12, 10);
-    assert( bowling_game_score() == 300 && "test_perfect_game()" );
-}
-
-static void test_one_strike(){
-    bowling_game_init();
-    bowling_game_roll(10);
-    bowling_game_roll(3);
-    bowling_game_roll(4);
-    roll_many(16, 0);
-    assert( bowling_game_score() == 24 && "test_one_strike()" );
-}
-
-static void test_one_spare(){
-    bowling_game_init();
-    bowling_game_roll(5);
-    bowling_game_roll(5);   /* spare */
-    bowling_game_roll(3);
-    roll_many(17, 0);
-    assert( bowling_game_score() == 16 && "test_one_spare()" );
-}
-
 static void test_gutter_game(){
     int i;
     bowling_game_init();
@@ -44,6 +20,30 @@ static void test_all_ones(){
     bowling_game_init();
     roll_many(20, 1);
     assert( bowling_game_score() == 20 && "test_all_ones");
+}
+
+static void test_one_spare(){
+    bowling_game_init();
+    bowling_game_roll(5);
+    bowling_game_roll(5);   /* spare */
+    bowling_game_roll(3);
+    roll_many(17, 0);
+    assert( bowling_game_score() == 16 && "test_one_spare()" );
+}
+
+static void test_one_strike(){
+    bowling_game_init();
+    bowling_game_roll(10);
+    bowling_game_roll(3);
+    bowling_game_roll(4);
+    roll_many(16, 0);
+    assert( bowling_game_score() == 24 && "test_one_strike()" );
+}
+
+static void test_perfect_game(){
+    bowling_game_init();
+    roll_many(12, 10);
+    assert( bowling_game_score() == 300 && "test_perfect_game()" );
 }
 
 int main(){
