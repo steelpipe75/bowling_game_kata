ce4999827834918a80b3600221dcdfcfb5161146
Nishizaki <yutaka.w1986@gmail.com>
2019-03-16 00:22:26 +0900

    ctest cli test

--- c_cmake_ninja/src/bowling_game_cli.c
+++ c_cmake_ninja/src/bowling_game_cli.c
@@ -1,23 +1,39 @@
 #include "bowling_game.h"
 #include <stdio.h>
 
-int main(){
+int main(int argc, char *argv[]){
     struct bowling_game * game;
-    game = bowling_game_create();
     int pins;
+    FILE *fp;
+    errno_t error;
 
-    while(1){
-        scanf("%d", &pins);
-        if(pins < 0){
-            break;
-        }else{
-            bowling_game_roll(game, pins);
+    if(argc == 2){
+        error = fopen_s(&fp, argv[1], "r");
+        if(error != 0){
+            printf_s("can't open %s\n", argv[1]);
+            return 1;
         }
+
+        game = bowling_game_create();
+
+        while(1){
+            fscanf(fp, "%d", &pins);
+            if(pins < 0){
+                break;
+            }else{
+                bowling_game_roll(game, pins);
+            }
+        }
+        fclose(fp);
+
+        printf_s("%d\n", bowling_game_score(game));
+
+        bowling_game_destroy(game);
+
+        return 0;
+    }else{
+        printf_s("usage: %s inputfilename\n", argv[0]);
+
+        return 1;
     }
-
-    printf("%d\n", bowling_game_score(game));
-
-    bowling_game_destroy(game);
-
-    return 0;
 }
