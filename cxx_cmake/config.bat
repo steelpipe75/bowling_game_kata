b10cb425c59b347862aaf3a1cb16e01b66650407
Nishizaki <yutaka.w1986@gmail.com>
2023-01-25 23:25:34 +0900

    Cコンパイラにclangを指定

--- cxx_cmake/config.bat
+++ cxx_cmake/config.bat
@@ -3,4 +3,4 @@ if not exist build (
     mkdir build
 )
 cd build
-cmd /K cmake -S .. -G Ninja -DCMAKE_CXX_COMPILER=clang++ %*
+cmd /K cmake -S .. -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ %*
