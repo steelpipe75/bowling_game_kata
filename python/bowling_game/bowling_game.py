d635396cbbc3dc5ef508c6360be929021feefa70
Nishizaki <steelpipe75@gmail.com>
2024-02-13 21:54:27 +0900

    Create bowling_game.py

---
+++ python/bowling_game/bowling_game.py
@@ -0,0 +1,13 @@
+class BowlingGame():
+    def __init__(self) -> None:
+        pass
+
+    def roll(self, pins: int) -> None:
+        pass
+
+    def score(self) -> int:
+        return 0
+
+if __name__ == '__main__':
+    print("Hello World!")
+    pass
