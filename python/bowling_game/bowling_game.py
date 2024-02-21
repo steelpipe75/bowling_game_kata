e7bacc4a928564416dc5523f5c30a1c6421a0b23
Nishizaki <steelpipe75@gmail.com>
2024-02-21 20:00:35 +0900

    Pythonでボーリングのスコア計算

--- python/bowling_game/bowling_game.py
+++ python/bowling_game/bowling_game.py
@@ -1,13 +1,57 @@
 class BowlingGame():
     def __init__(self) -> None:
+        self.rolls = []
         pass
 
     def roll(self, pins: int) -> None:
+        self.rolls.append(pins)
         pass
 
     def score(self) -> int:
-        return 0
+        score = 0
+        frame_index = 0
+        for frame in range(10):
+            if self.is_strike(frame_index):
+                score += 10 + self.strike_bonus(frame_index)
+                frame_index += 1
+            elif self.is_spare(frame_index):
+                score += 10 + self.spare_bonus(frame_index)
+                frame_index += 2
+            else:
+                score += self.rolls[frame_index] + self.rolls[frame_index + 1]
+                frame_index += 2
+        return score
+
+    def is_strike(self, frame_index: int) -> bool:
+        return self.rolls[frame_index] == 10
+
+    def is_spare(self, frame_index: int) -> bool:
+        return self.rolls[frame_index] + self.rolls[frame_index + 1] == 10
+
+    def strike_bonus(self, frame_index: int) -> int:
+        return self.rolls[frame_index + 1] + self.rolls[frame_index + 2]
+
+    def spare_bonus(self, frame_index: int) -> int:
+        return self.rolls[frame_index + 2]
+
 
 if __name__ == '__main__':
-    print("Hello World!")
+    game = BowlingGame()
+
+    game.roll(10)
+    game.roll(10)
+    game.roll(10)
+    game.roll(10)
+    game.roll(10)
+    game.roll(10)
+    game.roll(10)
+    game.roll(10)
+    game.roll(10)
+    game.roll(10)
+    game.roll(10)
+    game.roll(10)
+
+    score = game.score()
+    print(score)
+
     pass
