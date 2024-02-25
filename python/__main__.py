2a9164ad5290a63bde4cc88fe5dccd9099fd7aa3
Nishizaki <steelpipe75@gmail.com>
2024-02-25 22:31:07 +0900

    python版のコマンドラインツールを用意

--- python/__main__.py
+++ python/__main__.py
@@ -1,22 +1,19 @@
+import sys
 import bowling_game.bowling_game as bg
 
 if __name__ == '__main__':
-    game = bg.BowlingGame()
-
-    game.roll(10)
-    game.roll(10)
-    game.roll(10)
-    game.roll(10)
-    game.roll(10)
-    game.roll(10)
-    game.roll(10)
-    game.roll(10)
-    game.roll(10)
-    game.roll(10)
-    game.roll(10)
-    game.roll(10)
-
-    score = game.score()
-    print(score)
-
-    pass
+    args = sys.argv
+    if len(args) == 2:
+        f = open( args[1], 'r')
+        datalist = f.readlines()
+        f.close()
+        game = bg.BowlingGame()
+        for data in datalist:
+            if int(data) > 0:
+                # print(data, end='')
+                game.roll(int(data))
+        print(game.score())
+        sys.exit(0)
+    else:
+        print('Usage: python ' + args[0] + '<inputfilename>')
+        sys.exit(1)
