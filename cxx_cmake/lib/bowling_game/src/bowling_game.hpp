1d3b512cce48ab17e8f54d049f6aeed4f38a0185
Nishizaki <steelpipe75@gmail.com>
2024-03-03 14:23:14 +0900

    MSVCでdll使うプログラムがうまくいかなかった問題を修正（C++）

--- cxx_cmake/lib/bowling_game/src/bowling_game.hpp
+++ cxx_cmake/lib/bowling_game/src/bowling_game.hpp
@@ -1,17 +1,13 @@
 #ifndef BOWLING_GAME_H
 #define BOWLING_GAME_H
 
-#if 0
-#include <stdbool.h>
-
-struct bowling_game;
-extern struct bowling_game * bowling_game_create(void);
-extern void bowling_game_destroy(struct bowling_game * game);
-extern void bowling_game_roll(struct bowling_game * game, int pins);
-extern int bowling_game_score(struct bowling_game * game);
+#ifdef _MSC_VER
+    #define DLL_EXPORT __declspec(dllexport)
+#else
+    #define DLL_EXPORT 
 #endif
 
-class bowling_game{
+class DLL_EXPORT bowling_game{
 private:
     static const int max_rolls = 21;
     int rolls[max_rolls] = { 0,};
