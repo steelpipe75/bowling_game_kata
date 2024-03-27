bf3069c1bf2b569f3893572b1836e68b7db89143
Nishizaki <steelpipe75@gmail.com>
2024-03-27 21:39:13 +0900

    Update bowling_game.go

--- go/bowling_game/bowling_game/bowling_game.go
+++ go/bowling_game/bowling_game/bowling_game.go
@@ -4,10 +4,40 @@ type BowlingGame struct {
 	rolls []int
 }
 
-func (bg BowlingGame) Roll(pins int) {
-	bg.rolls = append(bg.rolls, pins)
+func (bg *BowlingGame) Roll(pins int) {
+	(*bg).rolls = append(bg.rolls, pins)
 }
 
 func (bg BowlingGame) Score() int {
-	return 0
+	score := 0
+	frameIndex := 0
+	for frame := 0; frame < 10; frame++ {
+		if bg.isStrike(frameIndex) {
+			score += bg.strikeScore(frameIndex)
+			frameIndex++
+		} else if bg.isSpare(frameIndex) {
+			score += bg.spareScore(frameIndex)
+			frameIndex += 2
+		} else {
+			score += bg.rolls[frameIndex] + bg.rolls[frameIndex+1]
+			frameIndex += 2
+		}
+	}
+	return score
+}
+
+func (bg BowlingGame) isStrike(frameIndex int) bool {
+	return (bg.rolls[frameIndex] == 10)
+}
+
+func (bg BowlingGame) isSpare(frameIndex int) bool {
+	return ((bg.rolls[frameIndex] + bg.rolls[frameIndex+1]) == 10)
+}
+
+func (bg BowlingGame) strikeScore(frameIndex int) int {
+	return 10 + bg.rolls[frameIndex+1] + bg.rolls[frameIndex+2]
+}
+
+func (bg BowlingGame) spareScore(frameIndex int) int {
+	return 10 + bg.rolls[frameIndex+2]
 }
