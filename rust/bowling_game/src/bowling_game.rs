83d2458564ad83cd77d85478c57ee402398a19c9
Nishizaki <steelpipe75@gmail.com>
2024-04-23 22:50:54 +0900

    Update bowling_game.rs

--- rust/bowling_game/src/bowling_game.rs
+++ rust/bowling_game/src/bowling_game.rs
@@ -49,3 +49,59 @@ impl BowlingGame {
         10 + self.rolls[frame_index + 2]
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_score_all_zero() {
+        let mut bg = BowlingGame::new();
+        for _ in 0..20 {
+            bg.roll(0);
+        }
+        assert_eq!(bg.score(), 0);
+    }
+
+    #[test]
+    fn test_score_all_ones() {
+        let mut bg = BowlingGame::new();
+        for _ in 0..20 {
+            bg.roll(1);
+        }
+        assert_eq!(bg.score(), 20);
+    }
+
+    #[test]
+    fn test_score_one_spare() {
+        let mut bg = BowlingGame::new();
+        bg.roll(5);
+        bg.roll(5);
+        bg.roll(3);
+        for _ in 0..17 {
+            bg.roll(0);
+        }
+        assert_eq!(bg.score(), 16);
+    }
+
+    #[test]
+    fn test_score_one_strike() {
+        let mut bg = BowlingGame::new();
+        bg.roll(10);
+        bg.roll(3);
+        bg.roll(4);
+        for _ in 0..16 {
+            bg.roll(0);
+        }
+        assert_eq!(bg.score(), 24);
+    }
+
+    #[test]
+    fn test_score_perfect_game() {
+        let mut bg = BowlingGame::new();
+        for _ in 0..12 {
+            bg.roll(10);
+        }
+        assert_eq!(bg.score(), 300);
+    }
+}
