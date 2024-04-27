61ca2df4741a75bed14b71aa5748e9daea5abd2a
Nishizaki <steelpipe75@gmail.com>
2024-04-27 20:44:33 +0900

    Update bowling_game.js
    インデント見直し + 空行追加

--- javascript_bun/bowling_game/bowling_game.js
+++ javascript_bun/bowling_game/bowling_game.js
@@ -1,52 +1,53 @@
 class BowlingGame {
-    constructor() {
-        this.rolls = [];
+  constructor() {
+    this.rolls = [];
+  }
+
+  roll(pins) {
+    this.rolls.push(pins);
+  }
+
+  score() {
+    const maxIndex = this.rolls.length;
+    this.roll(0)
+    this.roll(0)
+
+    let score = 0;
+    let frameIndex = 0;
+    for (let frame = 0; frame < 10; frame++) {
+      if (frameIndex >= maxIndex) {
+        break;
+      }
+      if (this.isStrike(frameIndex)) {
+        score += this.strikeScore(frameIndex);
+        frameIndex++;
+      } else if (this.isSpare(frameIndex)) {
+        score += this.spareScore(frameIndex);
+        frameIndex += 2;
+      } else {
+        score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+        frameIndex += 2;
+      }
     }
 
-    roll(pins) {
-        this.rolls.push(pins);
-    }
+    return score;
+  }
 
-    score() {
-        const maxIndex = this.rolls.length;
-        this.roll(0)
-        this.roll(0)
-        let score = 0;
-        let frameIndex = 0;
-        for (let frame = 0; frame < 10; frame++) {
-            if (frameIndex >= maxIndex) {
-                break;
-            }
-            if (this.isStrike(frameIndex)) {
-                score += this.strikeScore(frameIndex);
-                frameIndex++;
-            } else if (this.isSpare(frameIndex)) {
-                score += this.spareScore(frameIndex);
-                frameIndex += 2;
-            } else {
-                score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
-                frameIndex += 2;
-            }
-        }
+  isStrike(frameIndex) {
+    return this.rolls[frameIndex] === 10;
+  }
 
-        return score;
-    }
+  isSpare(frameIndex) {
+    return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
+  }
 
-    isStrike(frameIndex) {
-        return this.rolls[frameIndex] === 10;
-    }
+  strikeScore(frameIndex) {
+    return 10 + this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+  }
 
-    isSpare(frameIndex) {
-        return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
-    }
-
-    strikeScore(frameIndex) {
-        return 10 + this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
-    }
-
-    spareScore(frameIndex) {
-        return 10 + this.rolls[frameIndex + 2];
-    }
+  spareScore(frameIndex) {
+    return 10 + this.rolls[frameIndex + 2];
+  }
 }
 
 module.exports = BowlingGame;
