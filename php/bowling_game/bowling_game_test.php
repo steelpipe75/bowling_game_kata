8758b2704b470b89947cee78addc0a455a703fd1
Yutaka Nishizaki <steelpipe75@gmail.com>
2024-04-30 23:13:50 +0000

    Update bowling_game_test.php

--- php/bowling_game/bowling_game_test.php
+++ php/bowling_game/bowling_game_test.php
@@ -5,6 +5,44 @@ require_once 'bowling_game.php';
 use PHPUnit\Framework\TestCase;
 
 class bowling_game_test extends TestCase {
+    public function testAllZerosGame() {
+        $game = new BowlingGame();
+        for ($i = 0; $i < 20; $i++) {
+            $game->roll(0);
+        }
+        $this->assertSame(0, $game->score());
+    }
+
+    public function testAllOnesGame() {
+        $game = new BowlingGame();
+        for ($i = 0; $i < 20; $i++) {
+            $game->roll(1);
+        }
+        $this->assertSame(20, $game->score());
+    }
+
+    public function testOneSpareGame() {
+        $game = new BowlingGame();
+        $game->roll(5);
+        $game->roll(5);
+        $game->roll(3);
+        for ($i = 0; $i < 17; $i++) {
+            $game->roll(0);
+        }
+        $this->assertSame(16, $game->score());
+    }
+
+    public function testOneStrikeGame() {
+        $game = new BowlingGame();
+        $game->roll(10);
+        $game->roll(3);
+        $game->roll(4);
+        for ($i = 0; $i < 16; $i++) {
+            $game->roll(0);
+        }
+        $this->assertSame(24, $game->score());
+    }
+
     public function testPerfectGame() {
         $game = new BowlingGame();
         for ($i = 0; $i < 12; $i++) {
