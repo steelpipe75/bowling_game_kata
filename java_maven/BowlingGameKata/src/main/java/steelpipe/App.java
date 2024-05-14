589e7eb3061f14367553089e7046aa0565325293
Yutaka Nishizaki <steelpipe75@gmail.com>
2024-05-14 12:05:03 +0000

    "mvn archetype:generate"で作成したプロジェクトを追加

---
+++ java_maven/BowlingGameKata/src/main/java/steelpipe/App.java
@@ -0,0 +1,13 @@
+package steelpipe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
