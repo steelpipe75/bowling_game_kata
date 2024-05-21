bb95cec9a3858452945274699e1eddd711a917a3
Nishizaki <steelpipe75@gmail.com>
2024-05-21 23:31:42 +0900

    v言語でボーリングのスコア計算 コマンドライン引数で指定されたファイルを読み取って倒したピン数を設定する処理対応

--- v/bowling_game/src/main.v
+++ v/bowling_game/src/main.v
@@ -1,38 +1,34 @@
 module main
 import bowling_game
+import os
 
 fn main() {
+	// コマンドライン引数をチェック
+	if os.args.len < 2 {
+		eprintln('Usage: ${os.args[0]} <filename>')
+		exit(1)
+	}
+	filename := os.args[1]
+
+	// ファイルをReadモードで開く
+	file := os.read_file(filename) or {
+		eprintln('Failed to open file: $err')
+		exit(2)
+	}
+
 	mut bg := bowling_game.new_bowling_game()
 
-	bg.roll(1)
-	bg.roll(1)
-
-	bg.roll(1)
-	bg.roll(1)
-
-	bg.roll(1)
-	bg.roll(1)
-
-	bg.roll(1)
-	bg.roll(1)
-
-	bg.roll(1)
-	bg.roll(1)
-
-	bg.roll(1)
-	bg.roll(1)
-
-	bg.roll(1)
-	bg.roll(1)
-
-	bg.roll(1)
-	bg.roll(1)
-
-	bg.roll(1)
-	bg.roll(1)
-
-	bg.roll(1)
-	bg.roll(1)
+	for line in file.split_into_lines() {
+		value_i64 := line.parse_int(10, 0) or {
+			eprintln('Invalid number: $line')
+			exit(3)
+		}
+		if value_i64 < 0 {
+			break
+		}
+		value_int := int(value_i64)
+		bg.roll(value_int)
+	}
 
 	println(bg.score())
 }
