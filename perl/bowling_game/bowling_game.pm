9294853ff3790f5fb10d5dd936a1dfbf78a6f382
Yutaka Nishizaki <steelpipe75@gmail.com>
2024-07-05 15:39:15 +0000

    perlでボーリングのスコア計算

---
+++ perl/bowling_game/bowling_game.pm
@@ -0,0 +1,59 @@
+package bowling;
+
+use strict;
+use warnings;
+
+my @rolls = ();
+my $current_roll = 0;
+
+sub roll {
+    my $pins = shift;
+    push @rolls, $pins;
+}
+
+sub score {
+    my $score = 0;
+    my $frame_index = 0;
+
+    for my $frame (1..10) {
+        if (is_strike($frame_index)) { # strike
+            $score += 10 + strike_bonus($frame_index);
+            $frame_index++;
+        } elsif (is_spare($frame_index)) { # spare
+            $score += 10 + spare_bonus($frame_index);
+            $frame_index += 2;
+        } else {
+            $score += sum_of_balls_in_frame($frame_index);
+            $frame_index += 2;
+        }
+    }
+
+    return $score;
+}
+
+sub is_strike {
+    my $frame_index = shift;
+    return $rolls[$frame_index] == 10;
+}
+
+sub is_spare {
+    my $frame_index = shift;
+    return $rolls[$frame_index] + $rolls[$frame_index + 1] == 10;
+}
+
+sub strike_bonus {
+    my $frame_index = shift;
+    return $rolls[$frame_index + 1] + $rolls[$frame_index + 2];
+}
+
+sub spare_bonus {
+    my $frame_index = shift;
+    return $rolls[$frame_index + 2];
+}
+
+sub sum_of_balls_in_frame {
+    my $frame_index = shift;
+    return $rolls[$frame_index] + $rolls[$frame_index + 1];
+}
+
+1;
---
+++ perl/bowling_game/main.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use FindBin;
+use lib "$FindBin::Bin";
+use bowling_game;
+
+my $file = shift @ARGV;
+die "Usage: $0 <file>\n" unless defined $file;
+
+open my $fh, '<', $file or die "Could not open file '$file': $!\n";
+
+while (my $line = <$fh>) {
+    chomp $line;
+    last if $line < 0; # 負の数が出たら終了
+    bowling::roll($line);
+}
+
+close $fh;
+
+my $score = bowling::score();
+print "$score\n";
